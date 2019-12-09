6f1b78a97dad4bd3e15b5ba4b3ae70e1435fbda8
Eashwar Ranganathan <eashwar@eashwar.com>
2019-12-08 21:55:15 -0800

    Use defaultdict

--- 2019/intcode/utils.py
+++ 2019/intcode/utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Dict
 
 Program = Dict[int, int]
@@ -5,4 +6,4 @@ Program = Dict[int, int]
 
 def load_program_from_string(data: str) -> Program:
     d = [int(i) for i in data.strip().split(',')]
-    return dict(zip(range(len(d)), d))
+    return defaultdict(int, enumerate(d))
