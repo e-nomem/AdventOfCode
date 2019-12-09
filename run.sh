3f79a3842d96d6c95d9c1d48d7183145312cd5f8
Eashwar Ranganathan <eashwar@eashwar.com>
2019-12-08 23:31:30 -0800

    Add simple run script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+YEAR=2019
+if [[ "$#" -eq 3 ]]; then
+    YEAR=$1
+    shift
+fi
+
+printf -v DAY '%02d' ${1:-1}
+SOLUTION=${2:-1}
+
+if [[ ! -d "$YEAR" ]] || [[ ! -d "$YEAR/day_$DAY" ]] || [[ ! -f "$YEAR/day_$DAY/solution$SOLUTION.py" ]]; then
+    echo "[ERROR] Unable to find $YEAR/day_$DAY/solution$SOLUTION.py" >&2
+    exit 1
+fi
+
+exec python3 -m ${YEAR}.day_${DAY}.solution${SOLUTION}
