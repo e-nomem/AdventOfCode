e774eacaf818e1354c80c0031c728a62d97683cd
Eashwar Ranganathan <eashwar@eashwar.com>
2019-12-09 20:20:21 -0800

    Better output for run.sh

--- run.sh
+++ run.sh
@@ -10,9 +10,17 @@ fi
 printf -v DAY '%02d' ${1:-1}
 SOLUTION=${2:-1}
 
-if [[ ! -f "$YEAR/day_$DAY/solution$SOLUTION.py" ]]; then
-    echo "[ERROR] Unable to find $YEAR/day_$DAY/solution$SOLUTION.py" >&2
-    exit 1
-fi
 
-exec python3 -m ${YEAR}.day_${DAY}.solution${SOLUTION}
+run() {
+    local fname
+    fname="$1/day_$2/solution$3.py"
+    if [[ ! -f "$fname" ]]; then
+        echo "[ERROR] Unable to find $fname" >&2
+        exit 1
+    else
+        echo "[INFO] Running $fname"
+    fi
+    exec python3 -m $1.day_$2.solution$3
+}
+
+run "$YEAR" "$DAY" "$SOLUTION"
