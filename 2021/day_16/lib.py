c0d2050042445dc7d3bb5d198729c65033139b6a
Eashwar Ranganathan <eashwar@eashwar.com>
2021-12-16 00:47:51 -0800

    Clean up day 16

---
+++ 2021/day_16/lib.py
@@ -0,0 +1,59 @@
+from typing import Callable
+from typing import Union
+
+
+class Packet:
+    def __init__(self, version: int, type: int, data: Union[int, list['Packet']]) -> None:
+        self.version = version
+        self.type = type
+        self.data = data
+
+
+def bitstream(data: str) -> Callable[[int], str]:
+    stream = (c for i in range(0, len(data), 2) for c in f'{int(data[i:i+2], 16):08b}')
+
+    def _helper(n: int) -> str:
+        return ''.join(next(stream) for _ in range(n))
+
+    return _helper
+
+
+def parse_packet(data: Callable[[int], str]) -> tuple[Packet, int]:
+    version = int(data(3), 2)
+    packet_type = int(data(3), 2)
+    bits_consumed = 6
+
+    if packet_type == 4:
+        # Literal value
+        continue_flag = '1'
+        num = ''
+        while continue_flag == '1':
+            continue_flag = data(1)
+            num += data(4)
+            bits_consumed += 5
+
+        return Packet(version, packet_type, int(num, 2)), bits_consumed
+
+    subpackets: list[Packet] = []
+    subpacket_bits_consumed = 0
+
+    len_type = data(1)
+    bits_consumed += 1
+
+    if len_type == '0':
+        # 15 bits of len data
+        subpacket_len = int(data(15), 2)
+        bits_consumed += 15
+        has_subpackets = lambda: subpacket_bits_consumed < subpacket_len
+    else:
+        # 11 bits of packet count
+        subpacket_count = int(data(11), 2)
+        bits_consumed += 11
+        has_subpackets = lambda: len(subpackets) < subpacket_count
+
+    while has_subpackets():
+        packet, consumed = parse_packet(data)
+        subpackets.append(packet)
+        subpacket_bits_consumed += consumed
+
+    return Packet(version, packet_type, subpackets), bits_consumed + subpacket_bits_consumed
--- 2021/day_16/solution2.py
+++ 2021/day_16/solution2.py
@@ -9,27 +9,10 @@ from os import path
 from typing import Callable
 from typing import Union
 
+from .lib import bitstream
+from .lib import Packet
+from .lib import parse_packet
 from lib.timing import benchmark
-
-# Can't be bothered to figure out the zero padding right now
-HEX_MAP = {
-    '0': '0000',
-    '1': '0001',
-    '2': '0010',
-    '3': '0011',
-    '4': '0100',
-    '5': '0101',
-    '6': '0110',
-    '7': '0111',
-    '8': '1000',
-    '9': '1001',
-    'A': '1010',
-    'B': '1011',
-    'C': '1100',
-    'D': '1101',
-    'E': '1110',
-    'F': '1111',
-}
 
 OPERATORS: list[Callable[[int, int], int]] = [
     add,
@@ -43,54 +26,6 @@ OPERATORS: list[Callable[[int, int], int]] = [
 ]
 
 
-class Packet:
-    def __init__(self, version: int, type: int, data: Union[int, list['Packet']]) -> None:
-        self.version = version
-        self.type = type
-        self.data = data
-
-
-def parse_packet(data: Callable[[int], str]) -> tuple[Packet, int]:
-    version = int(data(3), 2)
-    packet_type = int(data(3), 2)
-    bits_consumed = 6
-
-    if packet_type == 4:
-        # Literal value
-        continue_flag = '1'
-        num = ''
-        while continue_flag == '1':
-            continue_flag = data(1)
-            num += data(4)
-            bits_consumed += 5
-
-        return Packet(version, packet_type, int(num, 2)), bits_consumed
-
-    subpackets: list[Packet] = []
-    subpacket_bits_consumed = 0
-
-    len_type = data(1)
-    bits_consumed += 1
-
-    if len_type == '0':
-        # 15 bits of len data
-        subpacket_len = int(data(15), 2)
-        bits_consumed += 15
-        has_subpackets = lambda: subpacket_bits_consumed < subpacket_len
-    else:
-        # 11 bits of packet count
-        subpacket_count = int(data(11), 2)
-        bits_consumed += 11
-        has_subpackets = lambda: len(subpackets) < subpacket_count
-
-    while has_subpackets():
-        packet, consumed = parse_packet(data)
-        subpackets.append(packet)
-        subpacket_bits_consumed += consumed
-
-    return Packet(version, packet_type, subpackets), bits_consumed + subpacket_bits_consumed
-
-
 def process_packet(packet: Packet) -> int:
     if isinstance(packet.data, int):
         # Only happens for type 4 'literal' packets
@@ -101,18 +36,8 @@ def process_packet(packet: Packet) -> int:
 
 @benchmark(10)
 def puzzle(input_lines: list[str]) -> None:
-    data = input_lines[0].strip()
-    bitstream = (b for c in data for b in HEX_MAP[c])
-
-    def read(n: int) -> str:
-        return ''.join(next(bitstream) for _ in range(n))
-
-    packet, _ = parse_packet(read)
-
-    # Sanity check that we processed all the data
-    remaining_data = [b for b in bitstream]
-    assert int(''.join(remaining_data), 2) == 0, 'Leftover data should only be zero padding, found actual data'
-    assert len(remaining_data) < 8, f'There should be less than 1 byte of zero padding at the end, found {len(remaining_data)} bits'
+    data = bitstream(input_lines[0].strip())
+    packet, _ = parse_packet(data)
 
     print(process_packet(packet))
 
