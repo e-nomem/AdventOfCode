3665492b35d5799e80ff9041cd88c555984d0a36
Eashwar Ranganathan <eashwar@eashwar.com>
2021-12-24 20:39:23 -0800

    Better time formatting

--- lib/timing.py
+++ lib/timing.py
@@ -5,6 +5,19 @@ from statistics import stdev
 from time import perf_counter_ns
 from typing import Optional
 
+UNITS = (("ns", 1000), ("μs", 1000), ("ms", 1000), ("s", 60), ("m", 60), ("h", 24), ("d", None))
+
+
+def _format_time(time: int) -> str:
+    reduced_time: float = time
+    for unit, factor in UNITS:
+        if factor is None or not reduced_time / factor > 1:
+            break
+
+        reduced_time /= factor
+
+    return f"{reduced_time:.2f} {unit}"
+
 
 def benchmark(n: int, cleanup: Optional[Callable[[], None]] = None):
     if not getenv("ENABLE_BENCHMARK"):
@@ -20,17 +33,17 @@ def benchmark(n: int, cleanup: Optional[Callable[[], None]] = None):
                 start = perf_counter_ns()
                 f(*args, **kwargs)
                 end = perf_counter_ns()
-                timing.append((end - start) // 1000000)
+                timing.append(end - start)
 
             print()
             print("Statistics:")
             if len(timing) == 1:
-                print(f"-> Elapsed: {timing[0]}ms")
+                print(f"-> Elapsed: {_format_time(timing[0])}")
             else:
                 average = mean(timing)
                 deviation = stdev(timing, xbar=average)
-                print(f"-> Average: {average}ms")
-                print(f"-> StdDev: {deviation}ms")
+                print(f"-> Average: {_format_time(average)}")
+                print(f"-> StdDev: {_format_time(deviation)}")
 
         return _helper
 
